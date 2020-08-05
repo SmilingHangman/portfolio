d617a1ed39d68123315a8c5da3577184b7563289
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-08-05 17:46:32 +0300

    Updates

---
+++ precache-manifest.748cfe6c4f3cf571871e0c23947614b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1107150b1ce44dd801f19e3de1cfdf2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4a2d1fc06abf69e70e17",
+    "url": "/portfolio/static/css/main.bef7b305.chunk.css"
+  },
+  {
+    "revision": "2fa2756bf8d14b774ad3",
+    "url": "/portfolio/static/js/2.3c1b1313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3c1b1313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2d1fc06abf69e70e17",
+    "url": "/portfolio/static/js/main.c856e3df.chunk.js"
+  },
+  {
+    "revision": "d9dd4d53728770834a4f",
+    "url": "/portfolio/static/js/runtime-main.0a880550.js"
+  },
+  {
+    "revision": "f31c2fb5abffb9edeb172cc2501a6c9f",
+    "url": "/portfolio/static/media/20200603_152906.f31c2fb5.jpg"
+  },
+  {
+    "revision": "98021aca8997af2fafda4460c0044748",
+    "url": "/portfolio/static/media/cog.98021aca.svg"
+  }
+]);
